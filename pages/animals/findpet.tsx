558c27e44e54d48d2baa54a62a32e1ff2e832efd
Eric Pezzulo <eric.pezzulo@gmail.com>
2022-03-02 02:00:44 -0500

    fixed page loading problem, still need to tweak search by filter function

--- pages/animals/findpet.tsx
+++ pages/animals/findpet.tsx
@@ -27,6 +27,7 @@ const FETCH_ALL_ANIMALS = gql`
 `;
 
 const findpet = () => {
+  const { data: animalData, loading: animalLoading } = useQuery(FETCH_ALL_ANIMALS);
   const [selectedDogAge, setSelectedDogAge] = useState("");
   const [selectedDogSize, setSelectedDogSize] = useState("");
   const [selectedDogBreed, setSelectedDogBreed] = useState("");
@@ -40,18 +41,21 @@ const findpet = () => {
     []
   );
   const router = useRouter();
-  const { data: animalData } = useQuery(FETCH_ALL_ANIMALS);
-  const catResults: string[] = animalData?.fetchAllAnimals.filter(
+   let catResults: any = animalData?.fetchAllAnimals.filter(
     (cat: any) => cat.species === "Cat"
   );
-  const dogResults: any = animalData?.fetchAllAnimals.filter(
+    let dogResults = animalData?.fetchAllAnimals.filter(
     (dog: any) => dog.species === "Dog"
-  );
+  )
+
+  useEffect(() => {
+    filterDogFunc()
+  }, [selectedDogAge, selectedDogBreed, selectedDogSize]);
   useEffect(() => {
     fetchDogBreeds();
     fetchCatBreeds();
-    filterDogFunc();
-  }, [selectedDogAge, selectedDogBreed, selectedDogSize, dogBreeds, catBreeds]);
+    return
+  },[])
   const filterDogFunc = () => {
     if (!selectedDogBreed && !selectedDogAge && !selectedDogSize) {
       let dogs = dogResults?.filter((dog: any) => dog.species === "Dog");
@@ -97,23 +101,22 @@ const findpet = () => {
       });
       setDisplayDogFilteredResults(dogs);
     }
-  };
+  }
   const fetchCatBreeds = async () => {
-    const res: any = await axios.get(
+    const res = await axios.get(
       "https://api.thecatapi.com/v1/breeds?attach_breed=0"
     );
     setCatBreeds(res.data.map((breeds: any) => breeds.name));
-    setSelectedCatBreed(res.data[0].name);
   };
   const fetchDogBreeds = async () => {
-    const res: any = await axios.get("https://dog.ceo/api/breeds/list/all");
-    const data: any = res.data.message;
-    const listOfDogBreeds: any = Object.entries(data).map(
+    const res = await axios.get("https://dog.ceo/api/breeds/list/all");
+    const data = res.data.message;
+    let listOfDogBreeds:any = Object.entries(data).map(
       (breed: any) => breed[0].charAt(0).toUpperCase() + breed[0].substring(1)
     );
     setDogBreeds(listOfDogBreeds);
   };
-
+ 
   const displayDogResults = displayDogFilteredResults?.map(
     (dog: any, key: any) => {
       return (
@@ -146,37 +149,40 @@ const findpet = () => {
       );
     }
   );
+ console.log(selectedDogAge)
+ console.log(selectedDogBreed)
+ console.log(selectedDogSize)
+ console.log(displayDogFilteredResults)
+  // const displayCatResults = catResults?.map((cat: any, key: any) => {
+  //   return (
+  //     <div
+  //       key={key}
+  //       onClick={() => router.push(`/animals/${cat.id}`)}
+  //       className="group m-2 shadow max-w-md rounded-md hover:cursor-pointer hover:lg:drop-shadow-xl hover:lg:scale-105 lg:hover:relative duration-100 ease-in-out"
+  //     >
+  //       <div className="flex h-80 sm:h-36 lg:h-56 w-full">
+  //         <img
+  //           className="flex w-full object-cover"
+  //           src={cat.imageUrl}
+  //           alt={`${cat.name}'s avatar`}
+  //         />
+  //       </div>
+  //       <div className="hidden group-hover:lg:block group-hover:lg:absolute bg-purple-400 bottom-16 w-full text-white duration-100 font-Titillium-Web ease-in-out ">
+  //         <p className="text-center py-1">Weight: {cat.weight} lbs</p>
+  //         <p className="text-center py-1">{cat.gender}</p>
+  //       </div>
 
-  const displayCatResults = catResults?.map((cat: any, key: any) => {
-    return (
-      <div
-        key={key}
-        onClick={() => router.push(`/animals/${cat.id}`)}
-        className="group m-2 shadow max-w-md rounded-md hover:cursor-pointer hover:lg:drop-shadow-xl hover:lg:scale-105 lg:hover:relative duration-100 ease-in-out"
-      >
-        <div className="flex h-80 sm:h-36 lg:h-56 w-full">
-          <img
-            className="flex w-full object-cover"
-            src={cat.imageUrl}
-            alt={`${cat.name}'s avatar`}
-          />
-        </div>
-        <div className="hidden group-hover:lg:block group-hover:lg:absolute bg-purple-400 bottom-16 w-full text-white duration-100 font-Titillium-Web ease-in-out ">
-          <p className="text-center py-1">Weight: {cat.weight} lbs</p>
-          <p className="text-center py-1">{cat.gender}</p>
-        </div>
-
-        <div className="p-2 group-hover:bg-purple-500 duration-150 rounded-b-md group-hover:text-white">
-          <p className="text-xl text-center font-Titillium-Web capitalize">
-            {cat.name}
-          </p>
-          <p className="truncate text-center w-full font-Work-Sans capitalize">
-            {cat.breed}
-          </p>
-        </div>
-      </div>
-    );
-  });
+  //       <div className="p-2 group-hover:bg-purple-500 duration-150 rounded-b-md group-hover:text-white">
+  //         <p className="text-xl text-center font-Titillium-Web capitalize">
+  //           {cat.name}
+  //         </p>
+  //         <p className="truncate text-center w-full font-Work-Sans capitalize">
+  //           {cat.breed}
+  //         </p>
+  //       </div>
+  //     </div>
+  //   );
+  // });
   return (
     <Layout>
       <Head>
@@ -387,7 +393,11 @@ const findpet = () => {
                       onChange={setSelectedDogAge}
                     >
                       <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
-                        <span className="block truncate">{selectedDogAge}</span>
+                        <span className="block truncate">{selectedDogAge === "" ? (
+                            <p>All</p>
+                          ) : (
+                            selectedDogAge
+                          )}</span>
                         <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
                           <SelectorIcon
                             className="w-5 h-5 text-gray-400"
@@ -402,6 +412,36 @@ const findpet = () => {
                         leaveTo="opacity-0"
                       >
                         <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm z-50">
+                        <Listbox.Option
+                            className={({ active }) =>
+                              `cursor-default select-none relative py-2 pl-10 pr-4 ${
+                                active
+                                  ? "text-purple-900 bg-purple-100"
+                                  : "text-gray-900"
+                              }`
+                            }
+                            value=""
+                          >
+                            {({ selected }) => (
+                              <>
+                                <span
+                                  className={`block truncate ${
+                                    selected ? "font-medium" : "font-normal"
+                                  }`}
+                                >
+                                  All
+                                </span>
+                                {selected ? (
+                                  <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-purple-600">
+                                    <CheckIcon
+                                      className="w-5 h-5"
+                                      aria-hidden="true"
+                                    />
+                                  </span>
+                                ) : null}
+                              </>
+                            )}
+                          </Listbox.Option>
                           <Listbox.Option
                             className={({ active }) =>
                               `cursor-default select-none relative py-2 pl-10 pr-4 ${
@@ -677,7 +717,11 @@ const findpet = () => {
                     <Listbox value="" onChange={setSelectedDogSize}>
                       <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
                         <span className="block truncate">
-                          {selectedDogSize}
+                        {selectedDogSize === "" ? (
+                            <p>All</p>
+                          ) : (
+                            selectedDogSize
+                          )}
                         </span>
                         <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
                           <SelectorIcon
@@ -693,7 +737,36 @@ const findpet = () => {
                         leaveTo="opacity-0"
                       >
                         <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm z-50">
-                          <Listbox.Option
+                        <Listbox.Option
+                            className={({ active }) =>
+                              `cursor-default select-none relative py-2 pl-10 pr-4 ${
+                                active
+                                  ? "text-purple-900 bg-purple-100"
+                                  : "text-gray-900"
+                              }`
+                            }
+                            value=""
+                          >
+                            {({ selected }) => (
+                              <>
+                                <span
+                                  className={`block truncate ${
+                                    selected ? "font-medium" : "font-normal"
+                                  }`}
+                                >
+                                  All
+                                </span>
+                                {selected ? (
+                                  <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-purple-600">
+                                    <CheckIcon
+                                      className="w-5 h-5"
+                                      aria-hidden="true"
+                                    />
+                                  </span>
+                                ) : null}
+                              </>
+                            )}
+                          </Listbox.Option><Listbox.Option
                             className={({ active }) =>
                               `cursor-default select-none relative py-2 pl-10 pr-4 ${
                                 active
@@ -943,7 +1016,7 @@ const findpet = () => {
                 <div className="flex flex-col items-center sm:grid sm:grid-2 md:grid-cols-2 lg:grid-cols-3 w-full">
                   {selectedSpecies === "Dog"
                     ? displayDogResults
-                    : displayCatResults}
+                    : <div>Cats</div>}
                 </div>
               </div>
             </div>
