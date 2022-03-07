cc6683420d4dd3c792ce9af069616d0c28ef14d9
Eric Pezzulo <eric.pezzulo@gmail.com>
2022-03-07 16:07:25 -0500

    gh pages not working

--- graphql/context.ts
+++ graphql/context.ts
@@ -1,10 +1,10 @@
 import { PrismaClient } from "@prisma/client";
-import prisma from "../lib/prisma";
+import {prisma} from "../lib/prisma";
 
 export type Context = {
   prisma: PrismaClient;
 };
-export async function createContext({ req, res }): Promise<Context> {
+export async function createContext(): Promise<Context> {
   return {
     prisma,
   };
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
+declare const global: NodeJS.Global & { prisma?: PrismaClient };
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
-export default prisma;
+export const prisma = global.prisma || new PrismaClient();
+if (process.env.NODE_ENV === "development") global.prisma = prisma;
+
+// let prisma: PrismaClient;
+
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient();
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient();
+//   }
+//   prisma = global.prisma;
+// }
+// export default prisma;
