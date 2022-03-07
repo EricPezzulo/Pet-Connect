778773edf04f47a889b78d25d2f24a79a8dddfcf
Eric Pezzulo <eric.pezzulo@gmail.com>
2022-03-07 18:06:43 -0500

    back to original

--- components/ExpandableMenu.tsx
+++ components/ExpandableMenu.tsx
@@ -8,28 +8,25 @@ const ExpandableMenu = () => {
   return (
     <div className="flex bg-gray-100 drop-shadow md:border-x border-b md:border-purple-200 h-auto w-full md:w-48 md:rounded-b-lg">
       <ul className="flex flex-col w-full font-Work-Sans">
-        <button
-        type='submit'
+        <li
           onClick={() => router.push(`/users/${session.id}`)}
           className="py-2 flex pl-5 hover:bg-purple-200 hover:text-purple-800 text-gray-600 duration-150 ease-in-out hover:cursor-pointer text-2xl md:text-lg md:pl-0 md:justify-center border-b border-purple-200"
         >
           My Profile
-        </button>
-        <button
-        type='submit'
+        </li>
+        <li
           onClick={() => router.push(`/animals/createnewlisting`)}
           className="py-2 flex pl-5 hover:bg-purple-200 hover:text-purple-800 text-gray-600 duration-150 ease-in-out hover:cursor-pointer text-2xl md:text-lg md:pl-0 md:justify-center border-b border-purple-200"
         >
           Create Listing
-        </button>
+        </li>
 
-       <button
-       type="submit"
+       <li
           className="py-2 flex pl-5 hover:bg-purple-200 hover:text-purple-800 text-gray-600 duration-150 ease-in-out hover:cursor-pointer hover:md:rounded-b-lg text-2xl md:text-lg md:pl-0 md:justify-center"
           onClick={() => signOut()}
         >
           Sign Out
-        </button>
+        </li>
       </ul>
     </div>
   );
