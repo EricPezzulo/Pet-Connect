8155f4a4000642ec0ecbaea7c69e3b5d10d63166
Eric Pezzulo <eric.pezzulo@gmail.com>
2022-03-08 21:56:06 -0500

    switching

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const apolloClient = new ApolloClient({
-  uri: "http://pet-connect.vercel.app/api/graphql",
+  uri: "https://pet-connect.vercel.app/api/graphql",
   cache: new InMemoryCache(),
 });
 
